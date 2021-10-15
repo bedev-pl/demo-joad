6649ba0a5ca04f544b8acb583d5804ced8ff41a9
Tomasz Ceglarz <tomaszcegdev@gmail.com>
2021-10-15 19:40:30 +0200

    2nd

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
 class IndexController
 {
-
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return new JsonResponse('OK');
+    }
 }
